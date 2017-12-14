5e1d35c29043f3ffb1ec1294d2d2ef9f5725fda3
Mitch Phillips <phillm4@rpi.edu>
2017-12-13 21:43:11 -0500

    Create _init_nlc_path.py

---
+++ _init_nlc_path.py
@@ -0,0 +1,7 @@
+import os
+import sys
+
+path = os.path.dirname(__file__)
+sys.path.insert(0, os.path.join(path, 'lib'))
+sys.path.insert(0, os.path.join(path, 'lib/videoseg'))
+sys.path.insert(0, os.path.join(path, 'lib/videoseg/src'))
